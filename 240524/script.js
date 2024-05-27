8ef17ec3f550623b2ea93034cc94ea8e36de8529
yuyunsuk <yuyunsuk@hanmail.net>
2024-05-27 17:30:05 +0900

    240527PM

--- 240524/script.js
+++ 240524/script.js
@@ -19,4 +19,32 @@ cardArray.forEach((card)=>{
     })
 })
 
+//Event.stopPropagation() 추가하여,
+//이벤트 전파 방지 4가지
+// 1) event.preventDefault() 현재 이벤트의 기본 동작을 중단
+// 2) event.stopPropagation() 현재 이벤트가 상위로 전파되지 않도록 중단
+// 3) event.stopImmediatePropagation() 현재 이벤트가 상위뿐 아니라 현재 레벨에 걸린 다른 이벤트도 동작하지 않도록 중단
+// 4) return false => jQuery 를 사용시 두개 모두 수행한 것과 같음, 사용 안할시 event.preventDefault()와 같음
+
+let secElem = document.querySelector('#section-sample');
+let divElem = document.querySelector('#div-sample');
+let btn = document.querySelector('#div-sample>input[type="button"]');
+
+//Section 이벤트 생성
+secElem.addEventListener('click', e => {
+    alert('section 클릭!');
+});
+
+//Div 클릭 이벤트 생성
+divElem.addEventListener('click', e => {
+    alert('div 클릭!');
+});
+
+//Input 버튼 클릭 이벤트 생성
+btn.addEventListener('click', e => {
+	//자식 엘리먼트 이벤트에 추가!
+	e.stopPropagation();
+    alert('button 클릭!');
+});
+
 console.log("JavaScript End!!!");
